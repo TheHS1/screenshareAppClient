85436bf9e2948a052081b9526c7acd348b16320c
Harjot Singh <harjot.singh0712@gmail.com>
2024-05-06 16:59:20 -0700

    Formatting and more inputs

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 
 extern "C" {
-	    #include <libavcodec/avcodec.h>
-	    #include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
 }
 
 using namespace std;
@@ -36,9 +36,9 @@ void clean() {
 void display(AVCodecContext* ctx, AVPacket* pkt, AVFrame* frame, SDL_Rect* rect, SDL_Texture* texture, SDL_Renderer* renderer, double fpsrend) {
     int framenum = ctx->frame_number;
     SDL_UpdateYUVTexture(texture, rect,
-        frame->data[0], frame->linesize[0],
-        frame->data[1], frame->linesize[1],
-        frame->data[2], frame->linesize[2]);
+            frame->data[0], frame->linesize[0],
+            frame->data[1], frame->linesize[1],
+            frame->data[2], frame->linesize[2]);
     SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer, texture, NULL, rect);
     SDL_RenderPresent(renderer);
@@ -46,118 +46,118 @@ void display(AVCodecContext* ctx, AVPacket* pkt, AVFrame* frame, SDL_Rect* rect,
 
 static void decode(AVCodecContext *dec_ctx, AVFrame *frame, AVPacket *pkt)
 {
-	int ret;
+    int ret;
 
-	ret = avcodec_send_packet(dec_ctx, pkt);
-	if (ret < 0) {
-		exit(1);
-	}
+    ret = avcodec_send_packet(dec_ctx, pkt);
+    if (ret < 0) {
+        exit(1);
+    }
 
-	while (ret >= 0) {
-		ret = avcodec_receive_frame(dec_ctx, frame);
-		if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
-			return;
-		else if (ret < 0) {
-			exit(1);
-		}
+    while (ret >= 0) {
+        ret = avcodec_receive_frame(dec_ctx, frame);
+        if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+            return;
+        else if (ret < 0) {
+            exit(1);
+        }
 
-		display(c, pkt, frame, &rect, texture, renderer, 30.0);
-		fflush(stdout);
-	}
+        display(c, pkt, frame, &rect, texture, renderer, 30.0);
+        fflush(stdout);
+    }
 }
 
 int main(int argc, char **argv) {
-	// ffmpeg setup
+    // ffmpeg setup
 
-	pkt = av_packet_alloc();
-	if (!pkt) {
-		cout << "error allocating packet" << endl;
-		exit(1);
-	}
+    pkt = av_packet_alloc();
+    if (!pkt) {
+        cout << "error allocating packet" << endl;
+        exit(1);
+    }
 
-	codec = avcodec_find_decoder(AV_CODEC_ID_H264);
-	if(!codec) {
-		cout << "error finding codec" << endl;
-		exit(1);
-	}
+    codec = avcodec_find_decoder(AV_CODEC_ID_H264);
+    if(!codec) {
+	    cout << "error finding codec" << endl;
+	    exit(1);
+    }
 
-	parser = av_parser_init(codec->id);
-	if (!parser) {
-		cout << "Error finding parser" << endl;
-		exit(1);
-	}
+    parser = av_parser_init(codec->id);
+    if (!parser) {
+        cout << "Error finding parser" << endl;
+        exit(1);
+    }
 
-	c = avcodec_alloc_context3(codec);
-	if (!c) {
-		cout << "Could not allocate video context memory" << endl;
-		exit(1);
-	}
+    c = avcodec_alloc_context3(codec);
+    if (!c) {
+        cout << "Could not allocate video context memory" << endl;
+        exit(1);
+    }
 
-	if (avcodec_open2(c, codec, NULL) < 0) {
-		cout << "Could not open codec" << endl;
-		exit(1);
-	}
+    if (avcodec_open2(c, codec, NULL) < 0) {
+        cout << "Could not open codec" << endl;
+        exit(1);
+    }
 
-	frame = av_frame_alloc();
-	if (!frame) {
-		cout << "could not allocate video frame" << endl;
-		exit(1);
-	}
-	
-	// sdl setup
-	if (SDL_Init(0) == -1) {
-		cout << "SDL_Init: " << SDL_GetError();
-		exit(1);
-	}
-	if (SDLNet_Init() == -1) {
-		cout << "SDLNet_Init: " << SDLNet_GetError();
-		exit(1);
-	}
+    frame = av_frame_alloc();
+    if (!frame) {
+        cout << "could not allocate video frame" << endl;
+        exit(1);
+    }
 
-	screen = SDL_CreateWindow("screenShareApp", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-				1280, 720, SDL_WINDOW_OPENGL);
+    // sdl setup
+    if (SDL_Init(0) == -1) {
+        cout << "SDL_Init: " << SDL_GetError();
+        exit(1);
+    }
+    if (SDLNet_Init() == -1) {
+        cout << "SDLNet_Init: " << SDLNet_GetError();
+        exit(1);
+    }
 
-	renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED);
-	if (!renderer) {
-		clean();
-		SDL_Quit();
-		return -1;
-	}
-	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_IYUV,
-			SDL_TEXTUREACCESS_STREAMING | SDL_TEXTUREACCESS_TARGET,
-			1920, 1080);
-	if (!texture) {
-		clean();
-		SDL_Quit();
-		return -1;
-	}
+    screen = SDL_CreateWindow("screenShareApp", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            1280, 720, SDL_WINDOW_OPENGL);
 
-	rect.x = 0;
-	rect.y = 0;
-	rect.w = 1920;
-	rect.h = 1080;
-	SDL_RenderSetLogicalSize(renderer, 1920, 1080);
+    renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        clean();
+        SDL_Quit();
+        return -1;
+    }
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_IYUV,
+            SDL_TEXTUREACCESS_STREAMING | SDL_TEXTUREACCESS_TARGET,
+            1920, 1080);
+    if (!texture) {
+        clean();
+        SDL_Quit();
+        return -1;
+    }
 
-	bool done = false;
-	SDL_Event evt;
-	uint32_t windowID = SDL_GetWindowID(screen);
+    rect.x = 0;
+    rect.y = 0;
+    rect.w = 1920;
+    rect.h = 1080;
+    SDL_RenderSetLogicalSize(renderer, 1920, 1080);
 
-	cout << "Starting server..." << endl;
+    bool done = false;
+    SDL_Event evt;
+    uint32_t windowID = SDL_GetWindowID(screen);
+
+    cout << "Starting server..." << endl;
 
     const int MAX_SOCKETS = 1;
-	TCPsocket server;
-	IPaddress ip;
+    TCPsocket server;
+    IPaddress ip;
     SDLNet_SocketSet socket_set;
     TCPsocket sockets[MAX_SOCKETS];
-	if (SDLNet_ResolveHost(&ip, NULL, 8080) == -1) {
-		cout << "SDLNet_ResolveHost: " << SDLNet_GetError();
-		exit(1);
-	}
-	server = SDLNet_TCP_Open(&ip);
-	if (!server) {
-		cout << "SDLNet_TCP_Open: " << SDLNet_GetError();
-		exit(1);
-	}
+    if (SDLNet_ResolveHost(&ip, NULL, 8080) == -1) {
+        cout << "SDLNet_ResolveHost: " << SDLNet_GetError();
+        exit(1);
+    }
+    server = SDLNet_TCP_Open(&ip);
+    if (!server) {
+        cout << "SDLNet_TCP_Open: " << SDLNet_GetError();
+        exit(1);
+    }
 
     socket_set = SDLNet_AllocSocketSet(MAX_SOCKETS+1);
     if(socket_set == NULL) {
@@ -168,56 +168,94 @@ int main(int argc, char **argv) {
         cout << "Could not add server socket to set";
         exit(1);
     }
-	while (!done) {
+    while (!done) {
         while (SDL_PollEvent(&evt)) {
-            if(evt.type == SDL_QUIT)
-                done = true;
-            else if(evt.type == SDL_KEYDOWN) {
-	        if(sockets[0]) {
-			//string send = "a" + to_string('0' + htonl(evt.key.keysym.sym));
-			char ok = evt.key.keysym.sym;
+            switch(evt.type) {
+                case SDL_QUIT:
+                    done = true;
 
-			// Combine 'a' with the character and return as a char*
-			std::string combined_string = "0";
-			combined_string += ok;
+                case SDL_KEYDOWN:
+                    if(sockets[0]) {
+                        char c = evt.key.keysym.sym;
 
-			// Allocate memory for the resulting string (including null terminator)
-			char* send = new char[combined_string.length() + 1];
+                        // Combine '0' with the character and return as a char*
+                        std::string combined_string = "0";
+                        combined_string += c;
 
-			// Copy the combined string to the allocated memory
-			std::strcpy(send, combined_string.c_str());
+                        // Allocate memory for the resulting string (including null terminator)
+                        char* send = new char[combined_string.length() + 1];
 
-			int len = SDLNet_TCP_Send(sockets[0], send, sizeof(send));
-			if(len == 0) {
-				cout << SDLNet_GetError();
-			}
-		}
-            } else if (evt.type == SDL_MOUSEMOTION) {
-		    if(sockets[0]) {
-                string ok = "1" + to_string((float) evt.motion.x / 1920) + "a" + to_string((float) evt.motion.y / 1080);
-                char* send = new char[ok.length() + 1];
-                strcpy(send, ok.c_str());
-                if(evt.motion.x >= 0 && evt.motion.y >= 0 && evt.motion.x <=1920 && evt.motion.y<=1080) {
-                    cout << send << endl;
-                    int len = SDLNet_TCP_Send(sockets[0], send, ok.length()+1);
-                    if(len == 0) {
-                        cout << SDLNet_GetError();
+                        // Copy the combined string to the allocated memory
+                        std::strcpy(send, combined_string.c_str());
+
+                        int len = SDLNet_TCP_Send(sockets[0], send, sizeof(send));
+                        if(len == 0) {
+                            cout << SDLNet_GetError();
+                        }
                     }
-                }
-		    }
-            } else if(evt.type == SDL_MOUSEBUTTONDOWN) {
-		    if(sockets[0]) {
-			    string ok;
-			    if(evt.button.button == SDL_BUTTON_LEFT) {
-				    ok = "2";
-				    char* send = new char[ok.length() + 1];
-				    strcpy(send, ok.c_str());
-				    int len = SDLNet_TCP_Send(sockets[0], send, 1);
-				    if(len == 0) {
-					    cout << SDLNet_GetError();
-				    }
-			    }
-		    }
+
+                case SDL_MOUSEMOTION:
+                    if(sockets[0]) {
+                        string ok = "1" + to_string((float) evt.motion.x / 1920) + "a" + to_string((float) evt.motion.y / 1080);
+                        char* send = new char[ok.length() + 1];
+                        strcpy(send, ok.c_str());
+                        if(evt.motion.x >= 0 && evt.motion.y >= 0 && evt.motion.x <=1920 && evt.motion.y<=1080) {
+                            cout << send << endl;
+                            int len = SDLNet_TCP_Send(sockets[0], send, ok.length()+1);
+                            if(len == 0) {
+                                cout << SDLNet_GetError();
+                            }
+                        }
+                    }
+
+                case SDL_MOUSEBUTTONDOWN: 
+                    if(sockets[0]) {
+                        string opcode;
+                        switch(evt.button.button) {
+                            case SDL_BUTTON_LEFT: {
+                                opcode = "2";
+                                char* send = new char[opcode.length() + 1];
+                                strcpy(send, opcode.c_str());
+                                int len = SDLNet_TCP_Send(sockets[0], send, 1);
+                                if(len == 0) {
+                                    cout << SDLNet_GetError();
+                                }
+                            }
+                            case SDL_BUTTON_RIGHT: {
+                                opcode = "3";
+                                char* send = new char[opcode.length() + 1];
+                                strcpy(send, opcode.c_str());
+                                int len = SDLNet_TCP_Send(sockets[0], send, 1);
+                                if(len == 0) {
+                                    cout << SDLNet_GetError();
+                                }
+                            }
+                        }
+                    }
+                case SDL_MOUSEBUTTONUP: 
+                    if(sockets[0]) {
+                        string opcode;
+                        switch(evt.button.button) {
+                            case SDL_BUTTON_LEFT: {
+                                opcode = "4";
+                                char* send = new char[opcode.length() + 1];
+                                strcpy(send, opcode.c_str());
+                                int len = SDLNet_TCP_Send(sockets[0], send, 1);
+                                if(len == 0) {
+                                    cout << SDLNet_GetError();
+                                }
+                            }
+                            case SDL_BUTTON_RIGHT: {
+                                opcode = "5";
+                                char* send = new char[opcode.length() + 1];
+                                strcpy(send, opcode.c_str());
+                                int len = SDLNet_TCP_Send(sockets[0], send, 1);
+                                if(len == 0) {
+                                    cout << SDLNet_GetError();
+                                }
+                            }
+                        }
+                    }
             }
         }
 
@@ -250,9 +288,7 @@ int main(int argc, char **argv) {
             } else {
                 char message[INBUF_SIZE];
                 int len = SDLNet_TCP_Recv(sockets[0], message, INBUF_SIZE);
-                if (!len) {
-                    break;
-                }
+
                 /* print out the message */
                 uint8_t *data = (uint8_t*) message;
                 size_t   data_size = len;
@@ -273,10 +309,10 @@ int main(int argc, char **argv) {
                 //printf("Received (%i): \n", len);
             }
         }
-	}
+    }
 
     SDLNet_TCP_Close(sockets[0]);
-	clean();
-	SDLNet_Quit();
-	SDL_Quit();
+    clean();
+    SDLNet_Quit();
+    SDL_Quit();
 }
